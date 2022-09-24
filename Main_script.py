495953a8fa998647e38890676506bc614090055f
Eduardo <trejo.alfredo.es@outlook.com>
2022-09-24 10:37:42 -0500

    Was adjusted the model user script

--- Main_script.py
+++ Main_script.py
@@ -2,6 +2,6 @@ from Trainer_Predicting_Esamble import Model_Trainer
 
 trainer_model = Model_Trainer()
 
-training_result=trainer_model.to_tain("Model/Model_Twttr_0_0","DataMinuteTwttr.csv",70)
+training_result=trainer_model.to_tain("Model/Model_Twttr_0_0","DataMinuteTwttr.csv",85)
 
 print(training_result)
