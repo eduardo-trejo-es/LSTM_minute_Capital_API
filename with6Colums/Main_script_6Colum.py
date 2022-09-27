1912f3b398f18416d1fc71138abb460f0335dd03
Eduardo <trejo.alfredo.es@outlook.com>
2022-09-27 09:19:09 -0500

    Model was  trainned

--- with6Colums/Main_script_6Colum.py
+++ with6Colums/Main_script_6Colum.py
@@ -4,8 +4,8 @@ from Forcaster_Model_6Colum import Forcast_Data
 trainer_model = Model_Trainer()
 forcaster = Forcast_Data("/Users/eduardo/Desktop/LSTM_minute_Capital_API/with6Colums/DataMinuteTwttrTimeColum.csv")
 
-#training_result=trainer_model.to_tain("/Users/eduardo/Desktop/LSTM_minute_Capital_API/with6Colums/Models/Model_Twttr_6colum_0_0","DataMinuteTwttrTimeColum.csv",80)
+#training_result=trainer_model.to_tain("/Users/eduardo/Desktop/LSTM_minute_Capital_API/with6Colums/Models/Model_Twttr_6colum_0_0","/Users/eduardo/Desktop/LSTM_minute_Capital_API/with6Colums/DataMinuteTwttrTimeColum.csv",80)
 
-Result=forcaster.ToForcast(14,"/Users/eduardo/Desktop/LSTM_minute_Capital_API/with6Colums/Models/Model_Twttr_6colum_0_0","2022-09-23T08:47:00")
+Result=forcaster.ToForcast(50,"/Users/eduardo/Desktop/LSTM_minute_Capital_API/with6Colums/Models/Model_Twttr_6colum_0_0","2022-08-18T09:00:00")
 
 print("done....")
