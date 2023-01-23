e07bbe99cf01bb43b44e5e6d6defe57087565b56
Eduardo <trejo.alfredo.es@outlook.com>
2023-01-22 18:25:45 -0600

    First forcast done

--- FFT_added_LSTM_BothInOut/ModelGen/Main_script.py
+++ FFT_added_LSTM_BothInOut/ModelGen/Main_script.py
@@ -2,11 +2,11 @@ from Trainer_Predicting_Esamble import Model_Trainer
 from Forcaster_Model import Forcast_Data
 
 trainer_model = Model_Trainer()
-#forcaster = Forcast_Data("DataMinuteTwttr.csv")
+forcaster = Forcast_Data("FFT_added_LSTM_BothInOut/DatasetGen/CRUDE_OIL/CRUDE_OIL_Dataand_FFT_10_50_100.csv")
 
 Model_Path="FFT_added_LSTM_BothInOut/ModelGen/Model/Model_LSTM_31_FFT"
 Data_CSV="FFT_added_LSTM_BothInOut/DatasetGen/CRUDE_OIL/CRUDE_OIL_Dataand_FFT_10_50_100.csv"
 
-training_result=trainer_model.to_train(Model_Path,Data_CSV,85)
+#training_result=trainer_model.to_train(Model_Path,Data_CSV,85)
 
-#Result=forcaster.ToForcast(14,"Model/Model_Twttr_0_0_Minutes")
+Result=forcaster.ToForcast(14,Model_Path)
