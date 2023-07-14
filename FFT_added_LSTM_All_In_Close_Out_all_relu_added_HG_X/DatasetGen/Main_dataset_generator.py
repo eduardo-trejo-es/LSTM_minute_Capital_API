a3df37aa6a0223327801cbdb094063f3cb857028
Eduardo <trejo.alfredo.es@outlook.com>
2023-07-13 18:35:32 -0600

    Solution
    FFT function can generate multiple Angle and Magnitud from different columns

--- FFT_added_LSTM_All_In_Close_Out_all_relu_added_HG_X/DatasetGen/Main_dataset_generator.py
+++ FFT_added_LSTM_All_In_Close_Out_all_relu_added_HG_X/DatasetGen/Main_dataset_generator.py
@@ -12,7 +12,7 @@ dateEnd= '2023-03-02'
 
 #######______________ CRUDE_Oil 'CL=F' _____________  ################
 itemName = 'CL=F'
-OneColum=True
+OneColum=False
 
 if OneColum:
     Original_Path_Retiving="FFT_added_LSTM_All_In_Close_Out_all_relu_added_HG_X/DatasetGen/CRUDE_OIL/OnlyCloseColum/CRUDE_OIL_Data.csv"
@@ -33,7 +33,7 @@ else:
     yearAddedPath="FFT_added_LSTM_All_In_Close_Out_all_relu_added_HG_X/DatasetGen/CRUDE_OIL/High_Low_Close/CRUDE_OIL_Data_And_year.csv"
     FFTAddedPath="FFT_added_LSTM_All_In_Close_Out_all_relu_added_HG_X/DatasetGen/CRUDE_OIL/High_Low_Close/CRUDE_OIL_CloseFFT_150_5Backdys.csv"
 
-
+"""
 
 #dataSet_Gen.RetivingDataPrices_Yahoo(itemName,dateStart, dateEnd,Original_Path_Retiving,Original_Path_Retiving)
 dataSet_Gen.UpdateToday(itemName,Original_Path_Retiving)
@@ -49,6 +49,7 @@ else:
 
 dataSet_Gen.PopListdf(columns,Original_Path_Retiving,Onlyonecolumn)
 
+
 #dataSet_Gen.AddColumnPRCNTG(Original_Path_Retiving,PRCNTGAddedPath)
 #if inversed:
 #    dataSet_Gen.AddColumnInverseDirePrice(Original_Path_Retiving,DirectionPrice)
@@ -62,40 +63,12 @@ dataSet_Gen.AddColumnMothandDay(DayNumAddedPath, MonthAddedPath,False)
 dataSet_Gen.AddColumnYear(MonthAddedPath,yearAddedPath)
 #
 #Generate new FFT columns done :)
+"""
 
-
-
-#Column=['Open_CL=F','High_CL=F','Low_CL=F','Close_CL=F','Volume_CL=F','Open_HG=F','High_HG=F','Low_HG=F','Close_HG=F','Volume_HG=F','Open_X','High_X','Low_X','Close_X','Volume_X']
-#Column=['Open','High','Low','Close']
-Column='Close'
-frec=[150]
-#frec=[10,50,100 
-inicialPath=yearAddedPath
-FFTNew_FileData=FFTAddedPath
-###### importante cambiar dependiendo del modelo y los backdays que considera    ##########
-#### backdays must be +1 to consider the 10 days like 11 for 10 back days, or 51 for 50 backdays
 backdaysToconsider=6
-firstDone=False
-for i in frec:
-    if firstDone==True:
-        inicialPath=FFTAddedPath
-        FFTNew_FileData=FFTAddedPath 
-    dataSet_Gen.getTheLastFFTValue(backdaysToconsider,i,Column,inicialPath, FFTNew_FileData)
-    firstDone=True
-    
-#to use:
-"""
-Column=['Open_CL=F','High_CL=F','Low_CL=F','Close_CL=F','Volume_CL=F','Open_HG=F','High_HG=F','Low_HG=F','Close_HG=F','Volume_HG=F','Open_X','High_X','Low_X','Close_X','Volume_X']
-Column=['Open','High','Low','Close','Volume']
-frec=[10,50,100]
-
 inicialPath=yearAddedPath
 FFTNew_FileData=FFTAddedPath
-for i in Column:
-    for j in frec:
-        if i == Column[0] and j == frec[0]:
-            dataSet_Gen.Add_ColumsFourier_Transform(j,i,inicialPath,FFTNew_FileData)
-        else:   
-            dataSet_Gen.Add_ColumsFourier_Transform(j,i,FFTNew_FileData,FFTNew_FileData)
-            
-"""
+Column=['High','Low']
+frec=[150]
+
+dataSet_Gen.getTheLastFFTValue(backdaysToconsider,frec,Column,inicialPath, FFTNew_FileData)     
